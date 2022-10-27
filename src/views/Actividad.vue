470d0485bddca3976fde17d76bd606c10e24605c
deividramirezarias <deivid.innovatec@gmail.com>
2022-10-27 10:09:36 -0500

    CF_06_135400_PROCESAMIENTO_INFORMACION_MERCADO
    CF_06_135400_PROCESAMIENTO_INFORMACION_MERCADO

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,18 +10,18 @@
     
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .titulo-segundo.color-acento-contenido
-        h2 Arrastrar y soltar
+        h2 Completar la frase
 
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/arrastrar_soltar.png", alt="Texto que describa la imagen")
+            img(src="@/assets/template/completar-espacios.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         p.mb-4 Apreciado aprendiz a continuación encontrará una actividad didáctica para afianzar los conceptos del componente de formación. Esta es una actividad de “completar la frase”, donde encontrará una frase en la cual debe diligenciar la palabra faltante de acuerdo a las definiciones vistas a lo largo del componente.Usted completara la frase los conceptos correspondientes. ¡Adelante y mucho ánimo!
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Arrastrar y soltar
+              p.fw-bold.mb-0 Completar la frase
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
                 span Realizar
